5c896e59feaed41f8a33cedc9bda402c7b8ae407
Juozas <juozasr@wix.com>
2020-10-31 22:12:22 +0200

    Added a check for  unique username

--- client/src/redux/modules/auth/authModule.ts
+++ client/src/redux/modules/auth/authModule.ts
@@ -1,16 +1,20 @@
-import {createReducer} from '@reduxjs/toolkit';
+import {createAction, createReducer} from '@reduxjs/toolkit';
 import {createThunk} from 'redux/helpers/thunks';
-import {AuthState, SIGN_IN, SIGN_OUT, SIGN_UP} from './types';
+import {AuthState, CLEAR_AUTH_STATE, SIGN_IN, SIGN_OUT, SIGN_UP} from './types';
 
 type SignUpUserPayload = {username: string; email: string; password: string};
 export const signUpUser = createThunk<SignUpUserPayload>(SIGN_UP, async (payload, firebase) => {
   const {username, email, password} = payload;
-  const auth = firebase().auth();
+  const {auth, firestore} = firebase();
+  const snapshot = await firestore().collection('users').where('username', '==', payload.username).get();
+  const resp = snapshot.docs.map((doc) => doc.data());
+  if (resp.length) {
+    throw {code: 'auth/username-exists'};
+  }
 
-  const authResponse = await auth.createUserWithEmailAndPassword(email, password);
-  await auth.signOut();
-  return await firebase()
-    .firestore()
+  const authResponse = await auth().createUserWithEmailAndPassword(email, password);
+  await auth().signOut();
+  return await firestore()
     .collection('users')
     .doc(authResponse.user?.uid)
     .set({username, email, isVerified: false, following: [], followerCount: 0, postCount: 0});
@@ -25,6 +29,8 @@ export const signOutUser = createThunk(SIGN_OUT, async (payload, firebase) => {
   await firebase().auth().signOut();
 });
 
+export const clearAuthState = createAction(CLEAR_AUTH_STATE);
+
 const defaultState = {
   error: '',
   loading: false,
@@ -33,6 +39,7 @@ const defaultState = {
 
 export const authReducer = createReducer<AuthState>(defaultState, (builder) =>
   builder
+    .addCase(clearAuthState, () => defaultState)
     .addCase(signUpUser.pending, (state) => {
       return {...defaultState, loading: true};
     })
