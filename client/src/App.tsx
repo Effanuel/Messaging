ebcb0d7acad084754ead3bac57682aa8f6c19c60
Juozas <juozasr@wix.com>
2020-11-01 21:48:07 +0200

    Added route for rendering user profiles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import {Navbar} from './components';
 import SignIn from 'container/auth/SignIn';
 import SignUp from 'container/auth/SignUp';
 import Home from 'container/home/Home';
+import Profile from 'container/profile/Profile';
 
 const App = React.memo(() => {
   return (
@@ -11,8 +12,9 @@ const App = React.memo(() => {
       <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/signin" component={SignIn} />
-        <Route exact path="/signup" component={SignUp} />
+        <Route path="/user/:id" component={Profile} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
       </Switch>
     </>
   );
