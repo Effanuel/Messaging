cf33e9861713adde8ec3a5f62046eae9df75c034
Juozas <juozasr@wix.com>
2020-11-01 21:48:16 +0200

    Removed logs

--- client/src/components/CardList/CardList.tsx
+++ client/src/components/CardList/CardList.tsx
@@ -8,13 +8,11 @@ interface Props {
 }
 
 function CardList({firestoreMessages}: Props) {
-  console.log('LISTST', firestoreMessages);
   return (
     <>
       {!_.isNil(firestoreMessages) &&
         firestoreMessages.map((message: any, index: number) => {
           const {username, text, createdAt} = message;
-          console.log(message, '0990');
           const date = moment(createdAt?.seconds * 1000).fromNow();
           return <MessageCard key={index} username={username} createdAt={date} type="card" text={text} />;
         })}
