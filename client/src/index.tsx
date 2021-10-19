d860a70dd3bee6c90dcfcbff6222031793990ac0
Juozas Rimantas <juozasr@wix.com>
2021-10-19 19:52:51 +0300

    Remove service worker

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,7 +5,6 @@ import {BrowserRouter} from 'react-router-dom';
 import {createTheme, ThemeProvider} from '@material-ui/core/styles';
 import App from './App';
 import {createStore} from 'redux/store/store';
-import * as serviceWorker from './serviceWorker';
 import './index.css';
 
 const theme = createTheme({
@@ -28,8 +27,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
